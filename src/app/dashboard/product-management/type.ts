cfe95b598713005bfa839a6c9c597b29580ecd23
ChanNguyen <giachan9@gmail.com>
2025-01-16 23:47:56 +0700

    add product service

--- src/app/dashboard/product-management/type.ts
+++ src/app/dashboard/product-management/type.ts
@@ -1,7 +1,18 @@
 export type Product = {
   id: number;
   name: string;
-  price: number;
+  price: string; // Keeping it as string, since it's passed as string in the example
   quantity: number;
-  image: string;
+  slug: string;
+  description?: string;
+  is_active: boolean;
+  is_deleted: boolean;
+  created_at: string;
+  images: { image: string }[]; // Array of image objects, assuming it's an array of URLs
+  category: {
+    id: number;
+    name: string;
+    slug: string;
+    is_deleted: boolean;
+  };
 };
